6cd449ec76dc2fe28a8abf08ea72e739a66f5cf8
Nguyen Phuc Loc <loc-np@fabrica-vietnam.com>
2015-11-09 11:39:06 +0700

    Set up project

---
+++ SampleCode/SampleCode/Extensions/UIViewControllerExtension.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewControllerExtension.swift
+//  SampleCode
+//
+//  Created by Nguyen Phuc Loc on 11/9/15.
+//  Copyright © 2015 locnp. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIView{
+    
+}
